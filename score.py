71dd795095becb47388ea3852539fc23c92535a5
Matt Watson <mattw50@outlook.com>
2024-04-08 09:34:32 -0400

    changes

--- score.py
+++ score.py
@@ -13,9 +13,7 @@ def init():
     global model
     # AZUREML_MODEL_DIR is an environment variable created during deployment.
     # It is the path to the model folder (./azureml-models/$MODEL_NAME/$VERSION)
-    model_path = os.path.join(
-        os.getenv("AZUREML_MODEL_DIR"), "sklearn_regression_model.pkl"
-    )
+    model_path = os.path.join(os.getenv("AZUREML_MODEL_DIR"), "model.pkl")
     # deserialize the model file back into a sklearn model
     model = joblib.load(model_path)
     logging.info("Init complete")
